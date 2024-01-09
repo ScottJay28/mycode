8508835e9f9dedf2117fa8de253ffcb842b28669
ScottJay28 <sjj.johnson28@gmail.com>
2024-01-09 23:14:12 +0000

    mixed lists

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+my_list = ["192.168.0.5", 5060, "UP"]
+print("the first item in the inst (IP): " + my_list[0] )
+print("the second item in the list (port): " + str(my_list[1]))
+print("the last item in the list (state): " + my_list[2])
+# display only the IP addresses to the screen.
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+print(f"IP addresses: {iplist[3]}, and {iplist[4]}")
+
